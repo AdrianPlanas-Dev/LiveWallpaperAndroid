320a7ebd13fb8b8aad6d3fef4012e2bdc61b4962
Adrian Planas <adrianplf@hotmail.com>
2026-09-23 19:06:41 +0200

    feat: add smooth day night lighting cycle

--- app/src/main/java/com/dynamicsky/livewallpaper/LandscapePreviewActivity.java
+++ app/src/main/java/com/dynamicsky/livewallpaper/LandscapePreviewActivity.java
@@ -89,13 +89,8 @@ public class LandscapePreviewActivity extends AppCompatActivity {
 
                         timeLabel.setText(time);
 
-                        if (hours >= 6 && hours < 17) {
-                            timeOverlay.setBackgroundColor(0x00000000);
-                        } else if (hours >= 17 && hours < 20) {
-                            timeOverlay.setBackgroundColor(0x66FF8A3D);
-                        } else {
-                            timeOverlay.setBackgroundColor(0x990B1645);
-                        }
+
+                        timeOverlay.setBackgroundColor(getOverlayColor(progress));
                     }
 
                     @Override
@@ -109,5 +104,49 @@ public class LandscapePreviewActivity extends AppCompatActivity {
         );
 
         backButton.setOnClickListener(v -> finish());
+    }
+    private int getOverlayColor(int minutes) {
+
+        if (minutes < 360) {
+            return 0x990B1645;
+        }
+
+        if (minutes < 480) {
+            float progress = (minutes - 360) / 120f;
+            int alpha = (int) (153 * (1f - progress));
+
+            return (alpha << 24) | 0x0B1645;
+        }
+
+        if (minutes < 1020) {
+            return 0x00000000;
+        }
+
+        if (minutes < 1200) {
+            float progress = (minutes - 1020) / 180f;
+            int alpha = (int) (102 * progress);
+
+            return (alpha << 24) | 0xFF8A3D;
+        }
+
+        if (minutes < 1320) {
+            float progress = (minutes - 1200) / 120f;
+
+            int orangeAlpha = (int) (102 * (1f - progress));
+            int blueAlpha = (int) (153 * progress);
+
+            int red = (int) (255 * (1f - progress) + 11 * progress);
+            int green = (int) (138 * (1f - progress) + 22 * progress);
+            int blue = (int) (61 * (1f - progress) + 69 * progress);
+
+            int alpha = Math.max(orangeAlpha, blueAlpha);
+
+            return (alpha << 24)
+                    | (red << 16)
+                    | (green << 8)
+                    | blue;
+        }
+
+        return 0x990B1645;
     }
 }
